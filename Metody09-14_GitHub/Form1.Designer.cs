ac358d9c0bed1b04f61a26526e43de12fdb8cf89
dostalm.04 <dostalm.04@A321-14>
2022-03-03 13:20:31 +0100

    Přidání metody diskriminant a její volání v programu

--- Metody09-14_GitHub/Form1.Designer.cs
+++ Metody09-14_GitHub/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -102,11 +103,22 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "c";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(394, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "vypočítej rovnici";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -125,6 +137,7 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Metody09-14_GitHub/Form1.cs
+++ Metody09-14_GitHub/Form1.cs
@@ -16,9 +16,26 @@ namespace Metody09_14_GitHub
         {
             InitializeComponent();
         }
+        double diskriminant(double a, double b, double c)
+        {
+            double vysledekD;
+            vysledekD = Math.Pow(b, 2) - (4 * a * c);
+            return vysledekD;
+
+        }
 
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            double c = double.Parse(textBox3.Text);
+            double diskrimi = diskriminant(a, b, c);
+            MessageBox.Show("Diskriminant je: " + diskrimi);
 
         }
     }
