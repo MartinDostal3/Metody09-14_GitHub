4a4d1e652a80f94dcda6e1936ddf8e5f1a5255c6
Dusty <martin.70cz@gmail.com>
2022-03-16 17:14:38 +0100

    Metody11

--- Metody09-14_GitHub/Form1.cs
+++ Metody09-14_GitHub/Form1.cs
@@ -34,7 +34,7 @@ namespace Metody09_14_GitHub
             return vysledekD;
 
         }
-         bool obsahujeCislici(string retezec, out int cifSoucet, out int cifSoucetLich, out int cifSoucetSud)
+        bool obsahujeCislici(string retezec, out int cifSoucet, out int cifSoucetLich, out int cifSoucetSud)
         {
             int cifra;
             cifSoucet = 0;
@@ -55,6 +55,26 @@ namespace Metody09_14_GitHub
             return obsahuje;
         }
 
+        int PocetSlov1(string retezec, out string novyretezec)
+        {
+            string novyretezec1 = retezec;
+            char[] separators = { ' ' };
+            string[] s = novyretezec1.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            int pocetSlov = s.Length;
+            int i = 0;
+            while (i < retezec.Length)
+            {
+                if (char.IsNumber(retezec[i]))
+                {
+                    retezec = retezec.Remove(i, 1);
+                }
+                else ++i;
+
+            }
+            novyretezec = retezec;
+            return pocetSlov;
+        }
+
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
 
@@ -62,14 +82,14 @@ namespace Metody09_14_GitHub
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+
             double a = double.Parse(textBox1.Text);
             double b = double.Parse(textBox2.Text);
             double c = double.Parse(textBox3.Text);
             double diskrimi = diskriminant(a, b, c);
             MessageBox.Show("Diskriminant je: " + diskrimi);
-           
-            
+
+
             if (diskrimi == 0)
             {
                 MessageBox.Show("Rovnice ma reseni v R");
@@ -87,5 +107,14 @@ namespace Metody09_14_GitHub
             if (obsahujeCislici(s, out cifSoucet, out cifSoucetLich, out cifSoucetSud)) MessageBox.Show("Obsahuje číslici\nCif Sou. je: " + cifSoucet + "\nCif. Sou. Lich. je: " + cifSoucetLich + "\nCif. Sou. Sud.: " + cifSoucetSud);
             else MessageBox.Show("Neobsahuje číslo");
         }
-    }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string s = "Ahoj 5já jsem 1 matěj12";
+            string novyretezec = "";
+            MessageBox.Show("Počet slov je: " + PocetSlov1(s, out novyretezec) + "\nŘetězec bez číslic: " + novyretezec);
+        }
+
+    } 
 }
+
